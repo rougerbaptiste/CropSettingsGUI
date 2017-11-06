c94bd4fc84776a62b8efe06eed0798e6900ad4b4
Baptiste Rouger <baptiste.rouger@cri-paris.org>
2017-11-06 16:50:12 +0100

    Added fitness files for selection part, setGenExpPlanA.py now works as expected

--- setGenExpPlanA.py
+++ setGenExpPlanA.py
@@ -3,36 +3,38 @@
 import csv
 from itertools import repeat
 
+folder = "test"
 expPlanFileName = "MyData.csv"
 
 generations = 30
 replicate = 10
-folder = "test"
 foldertime = 1
 initSize = 100
 nbPop = 100
-nbMarker = 100
-nbAllele = 5
-iniAllFreqEq = 0.2
+nbPopHalf= int(nbPop/2)
+nbMarker = 20
+nbAllele = 2
+iniAllFreqEq = 0.5
 fecundity = 2
 
 launcherFileName = "launcher"
 
-all1s = list(repeat(1,nbPop))
-half1 = list(repeat(0,nbPop/2)) + list(repeat(1,nbPop/2))
-continuous = list(range(0,nbPop))/nbPop
 
-launchFileString = "executable = cropmetapop\noutput=" + folder + ".out\nerror="\
-        + folder + ".err\nlog=" + folder + ".log\n\n"
+launcherFileCrop = "Universe=vanilla\nExecutable=usr/bin/python3\nshould_transfer_files=no\ninput=/dev/null\noutput=condor.out\nerror=condor.error\nlog=condor.log\nrequirements=( HAS_ASREML =?= False )\n#request_memory=8G\ngetenv=true\n"
+
+launcherFileR = "Universe=vanilla\nExecutable=usr/bin/R\nshould_transfer_files=no\ninput=/dev/null\noutput=condor.out\nerror=condor.error\nlog=condor.log\nrequirements=( HAS_ASREML =?= False )\n#request_memory=8G\ngetenv=true\n"
+
+all1s = ','.join(str(e) for e in list(repeat(1,nbPop)))
+tempHalf = list(repeat(0,nbPopHalf)) + list(repeat(1,nbPopHalf))
+half1 = ','.join(str(e) for e in tempHalf)
+continuous = ','.join(str(int(e/nbPop)) for e in list(range(0,nbPop)))
+
 paramNames = ["folder:", "generations:", "replicates:", "folder_time:",\
         "init_size:", "nb_pop:", "nb_marker:", "nb_allele:",\
         "init_AlleleFrequency_equal:", "fecundity:", "carr_capacity:",\
-        "percentSelf:", "mut_rate:", "nb_loc_sel:", "fitness:", "optimum:",\
-        "col_network:", "col_rate:", "col_directed:", "col_nb_edge:",\
-        "col_from_one:", "extinction:", "migr_network:", "migr_rate:",\
-        "migr_directed:", "migr_nb_edge:", "migr_from_one:", "migr_replace:"]
+        "percentSelf:", "mut_rate:", "fitness:", "optimum:"]
 
-paramMatrix = [[10,100,1000], [0,0.5,0.95],[0.001,0.01,0.1], [0,1,10],\
+paramMatrix = [[10,100,1000], [0,0.5,0.95],[0.001,0.01,0.1], ["0","fit1.csv","fit10.csv"],\
         [all1s, half1, continuous]]
 
 with open(expPlanFileName) as csvfile:
@@ -45,13 +47,18 @@ with open(expPlanFileName) as csvfile:
         stringToFile = ""
         for paramIndex, paramVal in enumerate(parameters):
             stringToFile = stringToFile + paramNames[paramIndex] + str(paramVal) + "\n"
-        fileNameToWrite = "_".join(list(map(str, list(parameters))))
+        fileNameToWrite = folder + '_'.join(row)
+        print(fileNameToWrite)
         fileToWrite = open(fileNameToWrite, "w")
         fileToWrite.write(stringToFile)
         fileToWrite.close()
 
-        launchFileString = launchFileString + "initialdir = " + fileNameToWrite + "\nqueue\n\n"
+        launcherFileCrop = launcherFileCrop + "\nArguments = /home/deap/aknainojika/cropmetapop/CropMetaPop.py /home/deap/aknainojika/" + fileNameToWrite + "\nqueue\n\n"
+        launcherFileR = launcherFileR + "\nArguments = --no-save -f /home/deap/aknainojika/main_initialisation.R\nqueue\n\n"
+launchFileCrop = open(launcherFileName, "w")
+launchFileCrop.write(launcherFileCrop)
+launchFileCrop.close()
 
-launcherFile = open(launcherFileName, "w")
-launcherFile.write(launchFileString)
-launcherFile.close()
+launchFileR = open(launcherFileName+"R", "w")
+launchFileR.write(launcherFileR)
+launchFileR.close()
