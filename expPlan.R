2b32fda4e89f44316ec8bb74a1abd570b24cc0f1
Baptiste Rouger <baptiste.rouger@cri-paris.org>
2017-11-07 13:21:37 +0100

    expPlan.R is now automatized : just enter the nb of parameters

--- expPlan.R
+++ expPlan.R
@@ -1,11 +1,21 @@
 rm(list=ls())
 library(planor)
-K=5 # nombre de paramètres
+K=9 # nombre de paramètres
 m=3 # nombre de niveau
 
 binf = 1+K*(m-1)+K*(K-1)/2*(m-1)^2 # calcul de la borne inférieure, ie nb minimum de simulations
 
-nbMin = m^(4) # on cherche une puissance de m t.q. m^x > borne inférieure
+print(binf)
+
+i = 0
+nbMin = m^(i) # on cherche une puissance de m t.q. m^x > borne inférieure
+while(nbMin < binf){
+    i = i + 1
+    nbMin = m^(i)
+}
+
+print(i)
+print(nbMin)
 
 plankey = planor.designkey(factors=LETTERS[1:K],nlevels = 3,resolution = 5,nunits = nbMin) # creation du plan
 plan=planor.design(plankey)
